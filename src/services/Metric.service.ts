7c1bfd966887d00f049dabe02d209f414ff81249
rodolfoHOk <rodolfo.okino@gmail.com>
2021-08-13 23:36:49 -0300

    add more methods MetricService

--- src/services/Metric.service.ts
+++ src/services/Metric.service.ts
@@ -15,6 +15,32 @@ class MetricService extends Service {
       .get<Metric.EditorMonthlyEarnings>(`/metrics/editor/monthly-earnings${queryString}`)
       .then(this.getData);
   }
+
+  static getMonthlyRevenuesExpenses(yearMonth: string) {
+    const queryString = generateQueryString({ yearMonth: yearMonth });
+    return this.Http
+      .get<Metric.MonthlyRevenuesExpenses>(
+        `/metrics/monthly-revenues-expenses${queryString}`,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      .then(this.getData);
+  }
+
+  static getMonthlyRevenuesExpensesChartJs(yearMonth: string) {
+    const queryString = generateQueryString({ yearMonth: yearMonth });
+    return this.Http
+      .get<Metric.MonthlyRevenuesExpensesChartjs>(
+        `/metrics/monthly-revenues-expenses${queryString}`,
+        {
+          headers: {
+            'Content-Type': 'application/vnd.alganews.chartjs+json'
+          }
+        })
+      .then(this.getData);
+  }
 }
 
 export default MetricService;
