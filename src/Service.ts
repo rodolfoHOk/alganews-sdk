9cca1b1b2275362849c67afb1c8f7c06e0b0c877
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-23 15:29:12 -0300

    identificando os tipos de erros

--- src/Service.ts
+++ src/Service.ts
@@ -1,7 +1,15 @@
 import axios, { AxiosResponse } from "axios";
+import handleAxiosResponseSuccess from './utils/handleAxiosResponseSuccess';
+import handleAxiosResponseError from './utils/handleAxiosResponseError';
 
 const Http = axios.create();
+
 Http.defaults.baseURL = 'http://localhost:8080';
+
+Http.interceptors.response.use(
+  handleAxiosResponseSuccess,
+  handleAxiosResponseError
+);
 
 function getData<T>(res: AxiosResponse<T>) {
   return res.data;
