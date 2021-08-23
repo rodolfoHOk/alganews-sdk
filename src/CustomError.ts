a78a2c6baff81dea6cc5355e7380cb198638ba0d
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-08-23 14:55:09 -0300

    mapeando erros da api

---
+++ src/CustomError.ts
@@ -0,0 +1,28 @@
+import { AlgaNews } from "./@types";
+
+
+export type ErrorData = AlgaNews.components['schemas']['Problem'];
+
+export type ErrorType = 
+  'BusinessError' 
+  | 'ForbiddenError'
+  | 'GenericError'
+  | 'IncomprehensibleMessageError'
+  | 'InvalidDataError'
+  | 'InvalidParameterError'
+  | 'ResourceNotFoundError'
+  | 'SystemError';
+
+class CustomError {
+  static type: ErrorType;
+
+  message?: string;
+  data?: ErrorData;
+
+  constructor(data: ErrorData) {
+    this.message = data.userMessage || data.detail;
+    this.data = data;
+  }
+}
+
+export default CustomError;
