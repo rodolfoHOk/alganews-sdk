77749a3a0da7dd538df859e8d55eef8b9fc51f26
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-13 22:48:31 -0300

    fix CashFlowQuery and CashFlowService

--- src/@types/CashFlow.ts
+++ src/@types/CashFlow.ts
@@ -1,18 +1,25 @@
 import { AlgaNews } from "./AlgaNews";
 
 export namespace CashFlow {
-  export type CategoryDetailed = AlgaNews.components['schemas']['CashFlowCategoryDetailed'];
-  export type CategoryIdInput = AlgaNews.components['schemas']['CashFlowCategoryIdInput'];
-  export type CategoryInput = AlgaNews.components['schemas']['CashFlowCategoryInput'];
-  export type CategoryMinimal = AlgaNews.components['schemas']['CashFlowCategoryMinimal'];
-  export type CategorySummary = AlgaNews.components['schemas']['CashFlowCategorySummary'];
-  export type EntryDetailed = AlgaNews.components['schemas']['CashFlowEntryDetailed'];
-  export type EntryInput = AlgaNews.components['schemas']['CashFlowEntryInput'];
-  export type EntrySummary = AlgaNews.components['schemas']['CashFlowEntrySummary'];
-  export type EntryType = AlgaNews.components['schemas']['CashFlowEntryType'];
+  export type CategoryDetailed =
+    AlgaNews.components["schemas"]["CashFlowCategoryDetailed"];
+  export type CategoryIdInput =
+    AlgaNews.components["schemas"]["CashFlowCategoryIdInput"];
+  export type CategoryInput =
+    AlgaNews.components["schemas"]["CashFlowCategoryInput"];
+  export type CategoryMinimal =
+    AlgaNews.components["schemas"]["CashFlowCategoryMinimal"];
+  export type CategorySummary =
+    AlgaNews.components["schemas"]["CashFlowCategorySummary"];
+  export type EntryDetailed =
+    AlgaNews.components["schemas"]["CashFlowEntryDetailed"];
+  export type EntryInput = AlgaNews.components["schemas"]["CashFlowEntryInput"];
+  export type EntrySummary =
+    AlgaNews.components["schemas"]["CashFlowEntrySummary"];
+  export type EntryType = AlgaNews.components["schemas"]["CashFlowEntryType"];
   export type Query = {
-    type: ['REVENUE' | 'EXPENSE'];
+    type: "REVENUE" | "EXPENSE";
     yearMonth: string;
-    sort: [keyof EntrySummary, 'asc' | 'desc'];
-  }
+    sort: [keyof EntrySummary, "asc" | "desc"];
+  };
 }
