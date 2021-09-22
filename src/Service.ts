e06537d8bb72f993d5323ceb069bd375c90e0172
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-22 14:58:41 -0300

    fix onRejected in setRequestInterceptors

--- src/Service.ts
+++ src/Service.ts
@@ -23,7 +23,7 @@ class Service {
     onFulfilled: (
       request: AxiosRequestConfig
     ) => AxiosRequestConfig | Promise<AxiosRequestConfig>,
-    onRejected: (error: any) => any
+    onRejected?: (error: any) => any
   ) {
     Http.interceptors.request.use(onFulfilled, onRejected);
   }
