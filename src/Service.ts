53dd3d8fed926040776310a57ee8663fb2416ec7
Rodolfo HiOk <rodolfo.okino@gmail.com>
2021-09-22 14:47:40 -0300

    add setRequestInterceptors

--- src/Service.ts
+++ src/Service.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosResponse } from "axios";
-import handleAxiosResponseSuccess from './utils/handleAxiosResponseSuccess';
-import handleAxiosResponseError from './utils/handleAxiosResponseError';
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import handleAxiosResponseSuccess from "./utils/handleAxiosResponseSuccess";
+import handleAxiosResponseError from "./utils/handleAxiosResponseError";
 
 const Http = axios.create();
 
-Http.defaults.baseURL = 'http://localhost:8080';
+Http.defaults.baseURL = "http://localhost:8080";
 
 Http.interceptors.response.use(
   handleAxiosResponseSuccess,
@@ -18,6 +18,15 @@ function getData<T>(res: AxiosResponse<T>) {
 class Service {
   protected static Http = Http;
   protected static getData = getData;
+
+  public static setRequestInterceptors(
+    onFulfilled: (
+      request: AxiosRequestConfig
+    ) => AxiosRequestConfig | Promise<AxiosRequestConfig>,
+    onRejected: (error: any) => any
+  ) {
+    Http.interceptors.request.use(onFulfilled, onRejected);
+  }
 }
 
 export default Service;
